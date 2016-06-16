d9eeded2e71bed97643abd888a8e0f7068cfbdb6
David Martinez <davidm251008@gmail.com>
2016-06-15 18:45:07 -0600

    Cache casi terminado

---
+++ MESI/include/Shcache.h
@@ -0,0 +1,17 @@
+#ifndef SHCACHE_H
+#define SHCACHE_H
+
+
+class Shcache
+{
+    public:
+        Shcache();
+        virtual ~Shcache();
+       	Shcache(const CPU& other);
+
+    protected:
+
+    private:
+};
+
+#endif // SHCACHE_H
---
+++ MESI/src/CPU.cpp
@@ -0,0 +1,16 @@
+#include "CPU.h"
+
+CPU::CPU()
+{
+    //ctor
+}
+
+CPU::~CPU()
+{
+    //dtor
+}
+
+CPU::CPU(const CPU& other)
+{
+    //copy ctor
+}
